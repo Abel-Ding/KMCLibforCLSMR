65529b7750e69429e6976aa6d0b766c3b5a03e35
Abel-Ding <almaorochiiori@163.com>
2018-06-20 14:22:20 +0800

    add arrunius reference(need to be corrected)

--- NiO_POM/NiO POM process.py
+++ NiO_POM/NiO POM process.py
@@ -10,7 +10,10 @@ Created on Wed Jun 13 16:09:01 2018
 
 import time
 from KMCLib import *
+from KMCLib.Analysis.TimeStepDistribution import TimeStepDistribution
+from KMCLib.Analysis.ProcessStatistics import ProcessStatistics
 from matplotlib import pyplot as plt
+
 
 t_calstart = time.clock()
 configuration = KMCConfigurationFromScript("config.py")
@@ -19,12 +22,18 @@ interactions  = KMCInteractionsFromScript("interactions.py")
 model = KMCLatticeModel(configuration=configuration, interactions=interactions)
 
 control_parameters = KMCControlParameters(number_of_steps=10000000,
-                                          dump_interval=10000)
+                                          dump_interval=10000,
+                                          analysis_interval=1)
+#set up the analysis
+tsd = TimeStepDistribution(binsize=0.001)
+pst = ProcessStatistics()
 
 # Run the simulation - save trajectory to 'NiO POM.py'
-model.run(control_parameters, trajectory_filename="NiO POM.py")
+model.run(control_parameters, trajectory_filename="NiO POM.py", analysis= [tsd,pst])
 t_calend = time.clock()
 print("sim complete time cost is :", t_calend-t_calstart) 
 #length of lattice
 size = 100
+
+
 
